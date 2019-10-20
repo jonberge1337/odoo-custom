ca8c5d453721be6095bb98a3286f7eeb85d081d4
jonberge1337 <jonbergerandi@gmail.com>
2019-10-20 16:53:57 +0200

    corregido los :

--- uni_jon/models/models.py
+++ uni_jon/models/models.py
@@ -11,13 +11,13 @@ from odoo import models, fields, api
     directorio = fields.Char(string="Direccion", size=70)
     email = fields.Char(size=40)
 
-class profesor_uni_jon(models.Model)
+class profesor_uni_jon(models.Model):
     _name = "profesor.uni_jon"
 
     ded = fields.Selection(string="Dedicacion", selection=[("Completa", 1), ("Parcial", 2), ("Colaboracion", 3)], size=40)
     imparte_en = fields.Many2many(comodel_name="centro.uni_jon", relation="uniProfe", colum1="idprofe", colum2="idcentro")
 
-class centro_uni_jon(models.Model)
+class centro_uni_jon(models.Model):
     _name = "centro.uni_jon"
     _rec_nombre = "nombrec"
 
@@ -25,14 +25,14 @@ class centro_uni_jon(models.Model)
     sus_alumnos = fields.One2many(comodel_name="alumno.uni_jon", inverse_name="su_centro", string="Alumnos matriculados")
     claustro = fields.Many2many(comodel_name="profesor.uni_jon", relation="uniProfe", colum1="idcentro", colum2="idprofe")
 
-class alumno_uni_jon(models.Model)
+class alumno_uni_jon(models.Model):
     _name = "alumno.uni_jon"
 
     exp = fields.Char(string="Expediente", required=True, size=40)
     titulacion = fields.Char(string="Titulacion", size=40)
     su_centro = fields.Many2one(comodel_name="centro.uni_jon", string="Estudia") 
 
-class personal_uni_jon(models.Model)
+class personal_uni_jon(models.Model):
     _name = "personal.uni_jon"
 
     unidad = fields.Char(string="Unidad",required=True, size=15)
