1d925f19f3fe6fa87f936a00b0fb1cbfdbdc30b9
jonberge1337 <jonbergerandi@gmail.com>
2019-10-28 22:12:45 +0100

    ejercicio publi_jon

--- publi_jon/models/models.py
+++ publi_jon/models/models.py
@@ -13,3 +13,36 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class revista_publi_jon(models.Model):
+    _name = "revista.publi_jon"
+
+    issn = fields.Char(string="Numero de identificación", required=True, size=20)
+    numero = fields.Integer(string="Numero")
+    titulo = fields.Char(string="Titulo", size=40)
+    año = fields.Date(string="Año")
+
+class articulo_publi_jon(models.Model):
+    _name = "articulo.publi_jon"
+
+    titulo = fields.Char(string="Titulo", size=30, required=True)
+    inicio = fields.Date(string="Pagina de inicio")
+    fin = fields.Date(string="Pagina final")
+    relacion = fields.Many2one(string="Publicados", comodel_name="revista.publi_jon")
+    
+class escrito_publi_jon(models.Model):
+    _name = "escrito.publi_jon"
+
+    titulo = fields.Many2one(comodel_name="articulo.publi_jon")
+    nombre = fields.Many2one(comodel_name="autor.publi_jon")
+    posicion = fields.Integer()
+
+
+class autor_publi_jon(models.Model):
+    _name = "autor.publi_jon"
+
+    nombre = fields.Char(string="Nombre", required=True, size=20)
+    email = fields.Char(string="Email", size=40)
+    adscrip = fields.Char(string="Adscripción", size=40)
+
+
