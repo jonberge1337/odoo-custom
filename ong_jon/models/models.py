cf01dd3425f296929dec7b7b1c8303a02c7d08fa
jonberge1337 <jonbergerandi@gmail.com>
2019-11-02 01:50:58 +0100

    hecho el models

--- ong_jon/models/models.py
+++ ong_jon/models/models.py
@@ -13,3 +13,41 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class sede_ong_jon(models.Model):
+    _name = 'sede.ong_jon'
+
+    id = fields.Integer(string="ID", required=True)
+    ciudad = fields.Char(string="Ciudad", size=40)
+    pais = fields.Char(string="Pais", size=40)
+    direccion = fields.Char(string="Dirección", size=50)
+    tfno = fields.Char(string="Telefono", size=12)
+    director = fields.Char(string="Nombre del director", size=40)
+
+class proyecto_ong_jon(models.Model):
+    _name = 'proyecto.ong_jon'
+
+    cod = fields.Char(string="Código", size=20, required=True)
+    titulo = fields.Char(string="Titulo", size=40)
+    inicio = fields.Date(string="Fecha de inicio")
+    fin = fields.Date(string="Fecha final")
+    presup = fields.Float(string="Presupuesto asignado", digits=2)
+    resp = fields.Char(string="Nombre del responsable", size=40)
+
+class actuacion_ong_jon(models.Model):
+    _name = 'actuacion.ong_jon'
+
+    cod = fields.Many2one(comodel_name="proyecto.ong_jon", string="Código del proyecto")
+    idpon = fields.Many2one(comodel_name="poblacio.ong_jon", string="Código de la población")
+    inversion = fields.Float(string="Inversion", digits=2)
+    descrip = fields.Char(string="Descripción", size=50)
+
+class poblacion_ong_jon(models.Model):
+    _name = 'poblacion.ong_jon'
+
+    idpob = fields.Integer(string="ID de la polación")
+    nombre = fields.Char(string="Nombre", size=40)
+    pais = fields.Char(string="Pais", size=40)
+    habitantes = fields.Integer()
+
+
